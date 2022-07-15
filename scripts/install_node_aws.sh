f8ef63281a5a7b192a1c78aa8abb2d6cd6533d77
KostiantynBielienko <konstantinbelenko36@gmail.com>
2022-07-15 12:02:37 +0000

    Added comments

--- scripts/install_node_aws.sh
+++ scripts/install_node_aws.sh
@@ -1,4 +1,17 @@
+# This script is a part of "/docs/aws-ami.md" documentation
+# Its purpose is to automatically download nodeJS to AWS Linux and install dependencies
+
+# To properly run this script - you should run it while being in the main repo folder
+# a.k.a. `[harbour-space-cloud-module] ./scripts/install_node_aws.sh`
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
+
+echo "INSATLLING NODE JS LTS"
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
+
+echo "INSTALLING DEPENDENCIES"
+npm i
+
+echo "SUCCESS!"
