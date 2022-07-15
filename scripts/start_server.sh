5b64643c479942e22223b914cf5fe32fe5a14eac
KostiantynBielienko <konstantinbelenko36@gmail.com>
2022-07-15 13:01:30 +0000

    Added a start_server shell script and modified app.service to use it

---
+++ scripts/start_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script is made to run server.js file on an AWS Linux
+# Do not use this script for anything else
+
+sudo /home/ec2-user/.nvm/versions/node/v16.16.0/bin/node /home/ec2-user/harbour-space-cloud-module/server.js
